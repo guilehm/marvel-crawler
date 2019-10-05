f1e3271880b37bd55d2a1f5c8059a060b5ef10d6
Guilherme Luis <guile.hm@hotmail.com>
2019-10-05 16:19:02 -0300

    create series spider

--- marvel/spiders/marvel.py
+++ marvel/spiders/marvel.py
@@ -2,7 +2,7 @@
 import json
 import os
 from urllib.parse import urlencode
-from marvel.items import CharacterItem
+from marvel.items import CharacterItem, SeriesItem
 import scrapy
 
 from marvel.utils import Marvel
@@ -16,10 +16,10 @@ LIMIT = 100
 BASE_URL = 'http://gateway.marvel.com/v1/public'
 
 
-def get_start_urls(endpoint, total, limit=LIMIT):
+def get_start_urls(endpoint, stop, start=0, limit=LIMIT):
     return [
         f'{BASE_URL}/{endpoint}?{urlencode(marvel.get_auth_data())}&limit={limit}&offset={offset}'
-        for offset in range(0, total + 1, limit)
+        for offset in range(start, stop + 1, limit)
     ]
 
 
@@ -28,6 +28,7 @@ class BaseSpider(scrapy.Spider):
     item = None
     known_quantity = None
     start_urls = []
+    start = 0
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -65,4 +66,11 @@ class CharacterSpider(BaseSpider):
     name = 'characters'
     item = CharacterItem
     known_quantity = int(os.getenv('KNOWN_QUANTITY_CHARACTERS'))
-    start_urls = get_start_urls('characters', total=known_quantity)
+    start_urls = get_start_urls('characters', start=0, stop=known_quantity)
+
+
+class SeriesSpider(BaseSpider):
+    name = 'series'
+    item = SeriesItem
+    known_quantity = int(os.getenv('KNOWN_QUANTITY_SERIES'))
+    start_urls = get_start_urls('series', start=0, stop=known_quantity)
