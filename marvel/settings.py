6deef3e9ed2d8d559f54f8d38381f4dd2e495e7c
Guilherme Luis <guile.hm@hotmail.com>
2019-10-05 14:42:20 -0300

    enable pipelines

--- marvel/settings.py
+++ marvel/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'marvel.spiders'
 #USER_AGENT = 'marvel (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'marvel.pipelines.MarvelPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'marvel.pipelines.MarvelPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
